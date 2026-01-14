7e70a046384dcec35a25505b6237bb4fa8dc4f6f
ivanokino <ivanokfadratov@gmail.com>
2026-01-15 01:47:17 +0300

    add pydantic val

--- src/Schemas/ProductSchemas.py
+++ src/Schemas/ProductSchemas.py
@@ -1,6 +1,6 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 
 class ProductSchema(BaseModel):
-    name: str
-    price: float
-    count: int    
+    name: str = Field(max_length=15, min_length=2) 
+    price: float = Field(ge=0)
+    count: int = Field(ge=1)
--- src/Schemas/UserSchemas.py
+++ src/Schemas/UserSchemas.py
@@ -1,7 +1,7 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 
 
 class UserSchema(BaseModel):
-    username: str
-    password: str
+    username: str = Field(max_length=15, min_length=1) 
+    password: str = Field(min_length=2, max_length=15)
     
